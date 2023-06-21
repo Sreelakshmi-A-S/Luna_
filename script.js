f2b1bdcad472bc82e84a3c04c6b8576451a31468
Sreelakshmi A S <sreelakshmias214@gmail.com>
2023-06-21 21:12:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
-// script.js
-
 function logPeriod() {
-    var startDate = document.getElementById('start-date-input').value;
-    var cycleLength = parseInt(document.getElementById('cycle-length-input').value);
-  
-    if (startDate && cycleLength) {
-      var lastPeriod = new Date(startDate);
-      var nextPeriod = calculateNextPeriod(lastPeriod, cycleLength);
-  
-      // Format the dates in "dd-mm" format
-      lastPeriod = formatDate(lastPeriod);
-      nextPeriod = formatDate(nextPeriod);
-  
-      // Update the UI with the retrieved data
-      document.getElementById('cycle-length').textContent = cycleLength;
-      document.getElementById('last-period').textContent = lastPeriod;
-      document.getElementById('next-period').textContent = nextPeriod;
-    }
+  var startDate = document.getElementById('start-date-input').value;
+  var cycleLength = parseInt(document.getElementById('cycle-length-input').value);
+
+  if (startDate && cycleLength) {
+    var lastPeriod = new Date(startDate);
+    var nextPeriod = calculateNextPeriod(lastPeriod, cycleLength);
+
+    // Format the dates in "dd-mm" format
+    lastPeriod = formatDate(lastPeriod);
+    nextPeriod = formatDate(nextPeriod);
+
+    // Update the UI with the retrieved data
+    document.getElementById('cycle-length').textContent = cycleLength;
+    document.getElementById('last-period').textContent = lastPeriod;
+    document.getElementById('next-period').textContent = nextPeriod;
+
+    // Render the calendar
+    renderCalendar(startDate, cycleLength);
   }
-  
-  function calculateNextPeriod(lastPeriod, cycleLength) {
-    var nextPeriodDate = new Date(lastPeriod);
-    nextPeriodDate.setDate(nextPeriodDate.getDate() + cycleLength);
-    nextPeriodDate.setHours(0, 0, 0, 0);
-    return nextPeriodDate;
-  }
-  
-  function formatDate(date) {
-    var day = date.getDate() -1;
-    var month = date.getMonth() + 1;
-    var year = date.getFullYear();
-  
-    day = day < 10 ? '0' + day : day;
-    month = month < 10 ? '0' + month : month;
-  
-    return day + '-' + month + '-' + year;
-  }
+}
+
+function calculateNextPeriod(lastPeriod, cycleLength) {
+  var nextPeriodDate = new Date(lastPeriod);
+  nextPeriodDate.setDate(nextPeriodDate.getDate() + cycleLength);
+  nextPeriodDate.setHours(0, 0, 0, 0);
+  return nextPeriodDate;
+}
+
+function formatDate(date) {
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+
+  day = day < 10 ? '0' + day : day;
+  month = month < 10 ? '0' + month : month;
+
+  return day + '-' + month + '-' + year;
+}
 
 function renderCalendar(periodStart, cycleLength) {
   var calendarContainer = document.getElementById("calendarContainer");
